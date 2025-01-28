b39acbc687841d0a2495ed1476d4816366c83181
LukaD45 <luka.dusko44@gmail.com>
2025-01-28 20:17:10 +0100

    number of views functionality

--- components/view.tsx
+++ components/view.tsx
@@ -1,13 +1,17 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "./ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  //TODO: MODIFY/UPDATE NUMBER OF VIEWS
+  await writeClient
+    .patch(id)
+    .set({ views: totalViews + 1 })
+    .commit();
 
   return (
     <div className="view-container">
