cbfdadfd9426586c863f6d59eee269c3dd0ca6c4
LukaD45 <luka.dusko44@gmail.com>
2025-01-28 14:05:03 +0100

    startup post body

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -21,6 +23,39 @@ const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
         <p className="tag">{formatDate(post._createdAt)}</p>
         <h1 className="heading">{post.title}</h1>
         <p className="sub-heading">{post.description}</p>
+      </section>
+
+      <section className="section_container ">
+        <img
+          src={post.image}
+          alt="Thumbnail"
+          className="w-full h-auto rounded-xl"
+        />
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${post.author?._id}`}
+              className="flex gap-2 items-center mb-3"
+            >
+              <Image
+                src={post.author.image}
+                alt="Avatar"
+                width={65}
+                height={65}
+                className="rounded-full drop-shadow-lg"
+              />
+              <div>
+                <p className="text-20-medium">{post.author.name}</p>
+                <p className="text-16-medium !text-black.300">
+                  {post.author.username}
+                </p>
+              </div>
+            </Link>
+            <p className="category-tag">{post.category}</p>
+          </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+          <p className="text-16-medium !text-black.300">{post.pitchDetails}</p>
+        </div>
       </section>
     </>
   );
