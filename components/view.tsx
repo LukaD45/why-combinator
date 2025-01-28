046cdb45fd20f037bbad914bd192785db0469124
LukaD45 <luka.dusko44@gmail.com>
2025-01-28 20:05:15 +0100

    views dynamic component

--- components/view.tsx
+++ components/view.tsx
@@ -1,13 +1,21 @@
+import { client } from "@/sanity/lib/client";
 import Ping from "./ping";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 
-const View = ({ id }: { id: string }) => {
+const View = async ({ id }: { id: string }) => {
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  //TODO: MODIFY/UPDATE NUMBER OF VIEWS
+
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
       <p className="view-text">
-        <span className="font-black">100 views</span>
+        <span className="font-black">Views:{totalViews} </span>
       </p>
     </div>
   );
