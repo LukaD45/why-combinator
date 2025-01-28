476fec1fc96775f67ed91a7607214550cbe15423
LukaD45 <luka.dusko44@gmail.com>
2025-01-28 19:59:05 +0100

    ping component

--- components/view.tsx
+++ components/view.tsx
@@ -1,7 +1,14 @@
-const View = () => {
+import Ping from "./ping";
+
+const View = ({ id }: { id: string }) => {
   return (
-    <div>
-      <p>View</p>
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+      <p className="view-text">
+        <span className="font-black">100 views</span>
+      </p>
     </div>
   );
 };
