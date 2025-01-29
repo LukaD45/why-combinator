421de31743a551e6275b667c543702ef243095a4
LukaD45 <luka.dusko44@gmail.com>
2025-01-29 07:07:24 +0100

    create startup page

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,11 @@
+const CreatePage = () => {
+  return (
+    <>
+      <section className="pink_container !min-h-[230px]">
+        <h1 className="heading">Submit your Startup</h1>
+      </section>
+    </>
+  );
+};
+
+export default CreatePage;
