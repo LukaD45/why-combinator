495813c5403f7c3d2f28ef80c9b83ed50d6297fe
LukaD45 <luka.dusko44@gmail.com>
2025-01-29 07:23:07 +0100

    input fields title, description, category, imageURL for startup form

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -1,9 +1,20 @@
-const CreatePage = () => {
+import { auth } from "@/auth";
+import StartupForm from "@/components/startup-form";
+import { redirect } from "next/navigation";
+
+const CreatePage = async () => {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
         <h1 className="heading">Submit your Startup</h1>
       </section>
+      <StartupForm />
     </>
   );
 };
