08d55e590ab6c13a5ac63c98a824007fb0956b13
LukaD45 <luka.dusko44@gmail.com>
2025-01-29 07:41:21 +0100

    using useActionState for managing state of the form

--- components/startup-form.tsx
+++ components/startup-form.tsx
@@ -1,11 +1,22 @@
 "use client";
 
-import { useState } from "react";
+import { useActionState, useState } from "react";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
+import MDEditor from "@uiw/react-md-editor";
+import { Button } from "./ui/button";
+import { Send } from "lucide-react";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
+  const [pitch, setPitch] = useState("");
+
+  const [state, formAction, isPending] = useActionState(
+    handleFormSsubmit,
+    error:"", status:"INITIAL"
+  );
+
+  const handleFormSsubmit = () => {};
   return (
     <form action={() => {}} className="startup-form">
       <div>
@@ -68,6 +79,37 @@ const StartupForm = () => {
 
         {errors.link && <p className="startup-form_error">{errors.link}</p>}
       </div>
+      <div data-color-mode="light">
+        <label htmlFor="pitch" className="startup-form_label">
+          Pitch
+        </label>
+        <MDEditor
+          value={pitch}
+          onChange={(value) => setPitch(value as string)}
+          id="pitch"
+          preview="edit"
+          height={300}
+          style={{ borderRadius: 20, overflow: "hidden" }}
+          textareaProps={{
+            placeholder:
+              "Briefly describe your idea and what problem it solves",
+          }}
+          previewOptions={{ disallowedElements: ["style"] }}
+        />
+
+        {errors.pitch && <p className="startup-form_error">{errors.pitch}</p>}
+      </div>
+      <Button
+        type="submit"
+        className="startup-form_btn text-white"
+        disabled={isPending}
+      >
+        {isPending ? "Submitting..." : "Submit your Pitch"}
+        <Send
+          className="size-6 ml-2 
+        "
+        />
+      </Button>
     </form>
   );
 };
