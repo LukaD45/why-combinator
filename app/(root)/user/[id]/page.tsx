0d84693d738bca7c0a78fc4c59ea37080edc518f
LukaD45 <luka.dusko44@gmail.com>
2025-01-30 15:10:02 +0100

    user page - profile card

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,18 +1,47 @@
 import { auth } from "@/auth";
 import { client } from "@/sanity/lib/client";
-import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const session = await auth();
-  const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
 
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
   if (!user) {
     return notFound();
   }
 
-  return <div>User Page</div>;
+  return (
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center line-clamp-1">
+              {user.name}
+            </h3>
+          </div>
+          <Image
+            src={user.image}
+            alt={user.name}
+            height={220}
+            width={220}
+            className="profile_image"
+          />
+          <p className="text-30-extrabold mt-7 text-center">@{user.username}</p>
+          <p className="mt-1 text-center text-14-normal">{user?.bio}</p>
+        </div>
+
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+          <p className="text-30-bold">
+            {session?.id === id ? "Your" : "All"}Startups
+          </p>
+          <ul className="card_grid-sm">{/* TODO: List all users startups*/}</ul>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default UserPage;
