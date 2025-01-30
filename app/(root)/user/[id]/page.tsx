558fcdb57072f1e274516f156003fab8e2e2db83
LukaD45 <luka.dusko44@gmail.com>
2025-01-30 14:49:40 +0100

    author_by_id_query query

---
+++ app/(root)/user/[id]/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { notFound } from "next/navigation";
+
+const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const id = (await params).id;
+  const session = await auth();
+  const user = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, { id });
+
+  if (!user) {
+    return notFound();
+  }
+
+  return <div>User Page</div>;
+};
+
+export default UserPage;
